0640c9ff2733f1d29ce2a9a4ce22c70f11fe665e
Petter Castro <petter.castro@domandtom.com>
2019-03-14 13:37:41 -0500

    Adding general serializer

--- app/src/main/scala/com/akkahttp/marshallers/JsonApiSupport.scala
+++ app/src/main/scala/com/akkahttp/marshallers/JsonApiSupport.scala
@@ -1,64 +1,59 @@
 package com.akkahttp.marshallers
 
-//import com.akkahttp.marshallers.JsonSupport.{cat, createCat, jsonFormat2}
 import org.zalando.jsonapi.{JsonapiRootObjectReader, JsonapiRootObjectWriter}
-import org.zalando.jsonapi.model.JsonApiObject.{BooleanValue, StringValue}
+import org.zalando.jsonapi.model.JsonApiObject.{BooleanValue, StringValue, NumberValue}
 import org.zalando.jsonapi.model.{Attribute, RootObject}
 import org.zalando.jsonapi.model.RootObject.{ResourceObject, ResourceObjects}
-import com.akkahttp.models.{Book, Cat, CreateCat, UpdateCat}
+import com.akkahttp.models.{Book, Cat, CreateCat}
 import spray.json.DefaultJsonProtocol
-
 import org.zalando.jsonapi.json.sprayjson.SprayJsonJsonapiProtocol._
 import spray.json._
 
+object JsonApiConverter extends DefaultJsonProtocol {
+  def convertTo[X](x: X): ResourceObject = {
+    val fields = (Map[String, Any]() /: x.getClass.getDeclaredFields) { (a, f) =>
+      f.setAccessible(true)
+      a + (f.getName -> f.get(x))}
 
-object JsonApiSupport extends DefaultJsonProtocol {
-
-  def catToRootObject(cat: Cat) = {
-    ResourceObject(
-      `type` = "cats",
-      attributes = Some(List(
-        Attribute("id", StringValue(cat.id)),
-        Attribute("name", StringValue(cat.name)),
-        Attribute("age", StringValue(cat.age))
-      ))
-    )
+    val attrs = fields map {
+      case(field, value) => value match {
+        case v:Double => Attribute(field, NumberValue(v))
+        case v:Int => Attribute(field, NumberValue(v))
+        case v:String => Attribute(field, StringValue(v))
+        case v:Boolean => Attribute(field, BooleanValue(v))
+        //        case v:_ => Attribute(field, JsObjectValue(v))
+      }
+    }
+    ResourceObject(`type` = x.getClass.getSimpleName.toLowerCase + "s", attributes = Some(attrs.toList))
   }
+}
 
-  def bookToRootObject(book: Book) = {
-    ResourceObject(
-      `type` = "books",
-      attributes = Some(List(
-        Attribute("id", StringValue(book.id)),
-        Attribute("title", StringValue(book.title)),
-        Attribute("description", StringValue(book.description)),
-        Attribute("sold", BooleanValue(book.sold))
-      ))
-    )
-  }
+
+object JsonApiSupport{
+  import JsonApiConverter._
 
   implicit val catJF: JsonapiRootObjectWriter[Cat] = new JsonapiRootObjectWriter[Cat] {
     override def toJsonapi(cat: Cat) = {
-      RootObject(data = Some(catToRootObject(cat)))
+      RootObject(data = Some(convertTo[Cat](cat)))
     }
   }
 
   implicit val seqCatJF: JsonapiRootObjectWriter[Seq[Cat]] = new JsonapiRootObjectWriter[Seq[Cat]] {
     override def toJsonapi(cats: Seq[Cat]) = {
-      val resources = cats map { cat => catToRootObject(cat) }
+      val resources = cats map { cat => convertTo[Cat](cat) }
       RootObject(data = Some(ResourceObjects(resources.toList)), links=None)
     }
   }
 
   implicit val bookJF: JsonapiRootObjectWriter[Book] = new JsonapiRootObjectWriter[Book] {
     override def toJsonapi(book: Book) = {
-      RootObject(data = Some(bookToRootObject(book)))
+      RootObject(data = Some(convertTo[Book](book)))
     }
   }
 
   implicit val seqBookJF: JsonapiRootObjectWriter[Seq[Book]] = new JsonapiRootObjectWriter[Seq[Book]] {
     override def toJsonapi(books: Seq[Book]) = {
-      val resources = books map { book => bookToRootObject(book)}
+      val resources = books map { book => convertTo[Book](book)}
       RootObject(data = Some(ResourceObjects(resources.toList)), links=None)
     }
   }
