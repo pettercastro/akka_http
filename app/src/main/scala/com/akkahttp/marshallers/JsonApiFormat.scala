912855800a139e94ecca876d3ddbe8b1361cc9bd
Petter Castro <petter.castro@domandtom.com>
2019-03-14 21:52:00 -0500

    Merging jsonwriter and jsonreader into jsonformat

---
+++ app/src/main/scala/com/akkahttp/marshallers/JsonApiFormat.scala
@@ -0,0 +1,57 @@
+package com.akkahttp.marshallers
+
+import org.zalando.jsonapi.JsonapiRootObjectFormat
+import org.zalando.jsonapi.json.sprayjson.SprayJsonJsonapiProtocol._
+import org.zalando.jsonapi.model.JsonApiObject.{BooleanValue, NumberValue, StringValue}
+import org.zalando.jsonapi.model.{Attribute, RootObject}
+import org.zalando.jsonapi.model.RootObject.{ResourceObject, ResourceObjects}
+import spray.json._
+
+object JsonApiFormat {
+  def convertTo[X](x: X): ResourceObject = {
+    val fields = (Map[String, Any]() /: x.getClass.getDeclaredFields) { (a, f) =>
+      f.setAccessible(true)
+      a + (f.getName -> f.get(x))}
+
+    val attrs = fields map {
+      case(field, value) => value match {
+        case v:Double => Attribute(field, NumberValue(v))
+        case v:Int => Attribute(field, NumberValue(v))
+        case v:String => Attribute(field, StringValue(v))
+        case v:Boolean => Attribute(field, BooleanValue(v))
+        //        case v:_ => Attribute(field, JsObjectValue(v))
+      }
+    }
+    ResourceObject(`type` = x.getClass.getSimpleName.toLowerCase + "s", attributes = Some(attrs.toList))
+  }
+
+  /**
+    * Serialize and deserialize a case class into a [RootObject], which is used to follow the json api specifications
+    * for requests and responses
+    * @param format: The JsonWriter and JsonReader used to serialize and deserialize the case class into a list
+    *              of attributes
+    * @tparam X
+    * @return
+    */
+  def jsonApiFormat[X](implicit format: JsonFormat[X]): JsonapiRootObjectFormat[X]= {
+    new JsonapiRootObjectFormat[X] {
+      override def toJsonapi(x: X): RootObject = {
+        x match {
+          case elements:Seq[X] =>
+            val resources = elements map { element => convertTo[X](element) }
+            RootObject(data = Some(ResourceObjects(resources.toList)), links=None)
+          case element:X =>
+            val res= element.toJson.convertTo[ResourceObject]
+            RootObject(data = Some(res))
+        }
+      }
+      override def fromJsonapi(rootObject: RootObject): X = {
+        rootObject.data.map {
+          case x:ResourceObject => x.toJson.asJsObject.getFields("attributes") match {
+            case Seq(JsObject(obj)) => JsObject(obj).convertTo[X]
+          }
+        }.get
+      }
+    }
+  }
+}
--- app/src/main/scala/com/akkahttp/marshallers/JsonApiSupport.scala
+++ app/src/main/scala/com/akkahttp/marshallers/JsonApiSupport.scala
@@ -1,68 +1,13 @@
 package com.akkahttp.marshallers
 
-import org.zalando.jsonapi.{JsonapiRootObjectReader, JsonapiRootObjectWriter}
-import org.zalando.jsonapi.model.JsonApiObject.{BooleanValue, NumberValue, StringValue}
-import org.zalando.jsonapi.model.{Attribute, RootObject}
-import org.zalando.jsonapi.model.RootObject.{ResourceObject, ResourceObjects}
 import com.akkahttp.models.{Book, Cat, CreateCat}
-import spray.json._
-
-object JsonApiConverter {
-  def convertTo[X](x: X): ResourceObject = {
-    val fields = (Map[String, Any]() /: x.getClass.getDeclaredFields) { (a, f) =>
-      f.setAccessible(true)
-      a + (f.getName -> f.get(x))}
-
-    val attrs = fields map {
-      case(field, value) => value match {
-        case v:Double => Attribute(field, NumberValue(v))
-        case v:Int => Attribute(field, NumberValue(v))
-        case v:String => Attribute(field, StringValue(v))
-        case v:Boolean => Attribute(field, BooleanValue(v))
-        //        case v:_ => Attribute(field, JsObjectValue(v))
-      }
-    }
-    ResourceObject(`type` = x.getClass.getSimpleName.toLowerCase + "s", attributes = Some(attrs.toList))
-  }
-
-  def jsonApiWriter[X](implicit writer: JsonWriter[X])= {
-    new JsonapiRootObjectWriter[X] {
-      override def toJsonapi(x: X) = {
-        x match {
-          case elements:Seq[X] =>
-            val resources = elements map { element => convertTo[X](element) }
-            RootObject(data = Some(ResourceObjects(resources.toList)), links=None)
-          case element:X =>
-            RootObject(data = Some(convertTo[X](element)))
-        }
-      }
-    }
-  }
-
-  def jsonApiReader[X](implicit reader: JsonReader[X])= {
-    new JsonapiRootObjectReader[X] {
-      override def fromJsonapi(rootObject: RootObject): X = {
-        import org.zalando.jsonapi.json.sprayjson.SprayJsonJsonapiProtocol._
-        val resourceObject = rootObject.data.map { case x:ResourceObject => x }.get
-
-        val attrs = resourceObject.toJson.asJsObject.getFields("attributes")
-        attrs match {
-          case Seq(JsObject(x)) => JsObject(x).convertTo[X]
-        }
-      }
-    }
-  }
-}
-
+import com.akkahttp.marshallers.JsonApiFormat._
+import com.akkahttp.marshallers.JsonSupport._
 
 object JsonApiSupport{
-  import JsonApiConverter._
-
-  import com.akkahttp.marshallers.JsonSupport._
-  implicit val catJW = jsonApiWriter[Cat]
-  implicit val seqCatJW = jsonApiWriter[Seq[Cat]]
-  implicit val bookJW = jsonApiWriter[Book]
-  implicit val seqBookJW = jsonApiWriter[Seq[Book]]
-
-  implicit val createCatJF = jsonApiReader[CreateCat]
+  implicit val catJW = jsonApiFormat[Cat]
+  implicit val seqCatJW = jsonApiFormat[Seq[Cat]]
+  implicit val bookJW = jsonApiFormat[Book]
+  implicit val seqBookJW = jsonApiFormat[Seq[Book]]
+  implicit val createCatJF = jsonApiFormat[CreateCat]
 }
